6f3c26e2d83a9eee2bb714f5304b61e1ff5fdf7d
this4you <mevladyslav@gmail.com>
2026-04-26 13:49:19 +0300

    Added GP

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/specialty-finder/',
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'specialty-finder.js'
+      }
+    }
+  },
   plugins: [react()],
 })
